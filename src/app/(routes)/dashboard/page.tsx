18942e58173a82b0689d90d406bc28b7f8a7edae
Mishel Mawn <mishel@Mishels-MacBook-Air.local>
2025-11-28 19:14:21 +0600

    remove dashboard view from outside routes folder

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/app/dashboard/layout/DashboardLayout";
+
+const page = () => {
+	return (
+		<DashboardLayout>
+			<div>Dashboard Home</div>
+		</DashboardLayout>
+	);
+};
+
+export default page;
