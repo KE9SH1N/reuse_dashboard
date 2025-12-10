c7e8830d8577d4cd9ebb79b86e52650cfc42590e
KE9SH1N <K3NSHI9@gmail.com>
2025-12-10 23:24:48 +0600

    update gselect and with search comp height

--- src/app/common/ui/GSelectWithSearch.tsx
+++ src/app/common/ui/GSelectWithSearch.tsx
@@ -59,7 +59,7 @@ const GSelectWithSearch: React.FC<GSelectWithSearchProps> = ({
 				<button
 					disabled={disabled}
 					className={cn(
-						"w-[300px] relative flex items-center justify-between border rounded-md px-3 py-1 text-left",
+						"w-[300px] relative flex items-center justify-between border rounded-md px-3 py-[4px] text-left",
 						"focus:outline-none",
 						className
 					)}
